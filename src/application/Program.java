3f8d8c1474669693ed1fbb47f3efca318e7d55f2
Marcio cley <marciocleydev@gmail.com>
2024-05-04 13:59:01 -0300

    first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import bordGame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("ola mundo");
-        System.out.println("hello world");
-
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
