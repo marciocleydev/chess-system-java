62259f63c29df32809a496dcd504fdc1dfd370ac
Marcio cley <marciocleydev@gmail.com>
2024-05-04 13:36:41 -0300

    segundo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,5 +4,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("ola mundo");
+        System.out.println("hello world");
+
     }
 }
